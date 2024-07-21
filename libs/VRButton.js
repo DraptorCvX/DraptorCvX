8b56a88a3f7343792ad1b44714130d18ee468062
DraptorCX <nvrlandgidle021@gmail.com>
2024-07-22 01:15:12 +0800

    Update VRButton.js

--- libs/VRButton.js
+++ libs/VRButton.js
@@ -104,7 +104,7 @@ class VRButton{
         //
 
         button.style.display = '';
-        button.style.right = 'calc(50% - 40px)';
+        button.style.right = '0px';
         button.style.width = '320px';
         button.style.cursor = 'pointer';
         button.innerHTML = '<i class="fas fa-vr-cardboard"></i>';
