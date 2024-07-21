0af778f46e97f5c8ffa3e039aadfb4abb9ccc5d7
DraptorCX <nvrlandgidle021@gmail.com>
2024-07-22 00:24:43 +0800

    Update Audio.js

--- libs/Audio.js
+++ libs/Audio.js
@@ -1,7 +1,4 @@
-﻿// Import the VRButton class (assuming it's in a separate file)
-import { VRButton } from './VRButton.js';
-
-const audioElement = document.createElement('audio');
+﻿const audioElement = document.createElement('audio');
 audioElement.src = 'C:/Users/ASUS/Documents/GitHub/DraptorCvX/audio/ENHYPENFatalTrouble.mp3'; // Replace with your path
 audioElement.loop = true; // Set loop playback by default
 
@@ -19,8 +16,3 @@ musicPlayPauseBtn.addEventListener('click', () => {
   }
   isMusicPlaying = !isMusicPlaying;
 });
-
-// Create the VR button instance and position it within the container
-const vrButtonContainer = document.getElementById('vr-button-container');
-const vrButton = new VRButton( /* renderer object */, /* options object */ );
-vrButtonContainer.appendChild(vrButton);
