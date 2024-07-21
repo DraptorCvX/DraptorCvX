5322258212f4b93fb3a45d11b9beda6a2fa666b9
DraptorCX <nvrlandgidle021@gmail.com>
2024-07-22 00:44:09 +0800

    sr

---
+++ libs/Audio.js
@@ -0,0 +1,18 @@
+﻿const audioElement = document.createElement('audio');
+audioElement.src = 'C:/Users/ASUS/Documents/GitHub/DraptorCvX/audio/ENHYPENFatalTrouble.mp3'; // Replace with your path
+audioElement.loop = true; // Set loop playback by default
+
+let isMusicPlaying = false;
+
+const musicPlayPauseBtn = document.getElementById('music-play-pause-btn');
+
+musicPlayPauseBtn.addEventListener('click', () => {
+  if (isMusicPlaying) {
+    audioElement.pause();
+    musicPlayPauseBtn.textContent = 'Play Music';
+  } else {
+    audioElement.play();
+    musicPlayPauseBtn.textContent = 'Pause Music';
+  }
+  isMusicPlaying = !isMusicPlaying;
+});
